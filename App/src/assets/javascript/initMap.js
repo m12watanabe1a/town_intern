9f0584981ccbfc4d11f0957ac2882161fd11e758
m12watanabe1a <m12watanabe1a@gmail.com>
2018-12-13 04:40:07 +0900

    modify map

--- App/src/assets/javascript/initMap.js
+++ App/src/assets/javascript/initMap.js
@@ -4,7 +4,11 @@ function initMap() {
 
     map = new google.maps.Map(document.getElementById('map'), {
         zoom: 12,
-        center: {lat: 35.681382, lng: 139.735861} //大井町を中心に設定
+        center: {lat: 35.681382, lng: 139.735861}, //大井町を中心に設定
+        mapTypeControl: false, //マップタイプ コントロール
+        fullscreenControl: false, //全画面表示コントロール
+        streetViewControl: false, //ストリートビュー コントロール
+        zoomControl: false, //ズーム コントロール
     });
 
     setup_searchBox();
